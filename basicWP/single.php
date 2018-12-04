82554a601bda87724e9756ec564fa0a04519a5bb
iQuixotic <treyw1850@gmail.com>
2018-12-04 13:48:40 -0600

    any massive scale

--- basicWP/single.php
+++ basicWP/single.php
@@ -5,9 +5,7 @@
             <?php while(have_posts()) : the_post(); ?>
             <article class="post">
                 <h3>
-                    <a href="<?php the_permalink(); ?> ">
                     <?php the_title(); ?>
-                    </a>
                 </h3>
                 <div class="meta">
                     Created By: <?php the_author(); ?> on <?php the_date(); ?>
@@ -17,11 +15,8 @@
                         <?php the_post_thumbnail(); ?>                
                     </div>
                 <?php endif; ?>
-                <?php the_excerpt(); ?>
+                <?php the_content(); ?>
                 <br>
-                <a class="button" href="<?php the_permalink(); ?> ">
-                    Read More
-                </a>
                 </article>
             <?php endwhile; ?>
         <?php else : ?>
@@ -30,4 +25,3 @@
         </div>
     </div>
 <?php get_footer(); ?>
-        
